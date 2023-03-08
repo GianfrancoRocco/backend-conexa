83a16e99e3dc3782b3d5023e2770d3202c2a4ced
gianfrancorocco.business@gmail.com <gianfrancorocco.business@gmail.com>
2023-03-08 16:51:59 -0300

    test: VehicleController

---
+++ tests/Feature/StarWarsApi/VehicleControllerTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Tests\Feature\StarWarsApi;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class VehicleControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_a_collection_of_vehicles_can_be_fetched(): void
+    {
+        $this->actingAs(User::factory()->create());
+
+        $response = $this->getJson(route('vehicle.index'));
+
+        $response->assertSuccessful();
+    }
+
+    public function test_can_switch_between_pages(): void
+    {
+        $this->actingAs(User::factory()->create());
+
+        $response = $this->getJson(route('vehicle.index', [
+            'page' => 2
+        ]));
+
+        $response->assertSuccessful();
+
+        $response = $this->getJson(route('vehicle.index', [
+            'page' => 3
+        ]));
+
+        $response->assertSuccessful();
+    }
+
+    public function test_one_vehicle_can_be_fetched(): void
+    {
+        $this->actingAs(User::factory()->create());
+
+        $response = $this->getJson(route('vehicle.show', [7]));
+
+        $response->assertSuccessful();
+    }
+
+    public function test_not_found_response_if_vehicle_does_not_exist(): void
+    {
+        $this->actingAs(User::factory()->create());
+
+        $response = $this->getJson(route('vehicle.show', [232323321]));
+
+        $response->assertNotFound();
+    }
+
+    public function test_unauthorized_response_if_not_authenticated(): void
+    {
+        $response = $this->getJson(route('vehicle.index'));
+
+        $response->assertUnauthorized();
+    }
+}
