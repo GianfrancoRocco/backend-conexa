76e16c90f37cac893e68550df383fb47ad46e582
gianfrancorocco.business@gmail.com <gianfrancorocco.business@gmail.com>
2023-03-08 16:50:50 -0300

    test: PlanetController

---
+++ tests/Feature/StarWarsApi/PlanetControllerTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Tests\Feature\StarWarsApi;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class PlanetControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_a_collection_of_planets_can_be_fetched(): void
+    {
+        $this->actingAs(User::factory()->create());
+
+        $response = $this->getJson(route('planet.index'));
+
+        $response->assertSuccessful();
+    }
+
+    public function test_can_switch_between_pages(): void
+    {
+        $this->actingAs(User::factory()->create());
+
+        $response = $this->getJson(route('planet.index', [
+            'page' => 2
+        ]));
+
+        $response->assertSuccessful();
+
+        $response = $this->getJson(route('planet.index', [
+            'page' => 3
+        ]));
+
+        $response->assertSuccessful();
+    }
+
+    public function test_one_planet_can_be_fetched(): void
+    {
+        $this->actingAs(User::factory()->create());
+
+        $response = $this->getJson(route('planet.show', [1]));
+
+        $response->assertSuccessful();
+    }
+
+    public function test_not_found_response_if_planet_does_not_exist(): void
+    {
+        $this->actingAs(User::factory()->create());
+
+        $response = $this->getJson(route('planet.show', [232323321]));
+
+        $response->assertNotFound();
+    }
+
+    public function test_unauthorized_response_if_not_authenticated(): void
+    {
+        $response = $this->getJson(route('planet.index'));
+
+        $response->assertUnauthorized();
+    }
+}
